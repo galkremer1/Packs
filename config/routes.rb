3b0dd91075c1924aaece38320d0bf951a6491a2a
galkremer1 <galkremer1@walla.co.il>
2015-08-03 20:58:08 +0300

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'reports/index'
+
   resources :amounts
   resources :products
   # The priority is based upon order of creation: first created -> highest priority.
