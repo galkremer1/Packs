4cc54b5839f2fea60a245dfba4e5856a2c142141
galkremer1 <galkremer1@walla.co.il>
2015-08-06 12:08:09 +0300

    more changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'welcome/homepage'
+
   resources :reports
   get 'reports/index'
 
@@ -8,7 +10,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#homepage'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
